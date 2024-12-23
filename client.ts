4edcbea603367791aa0629adc2b79fd4a2479f80
Madhukar Beema <mbeema@saavys.com>
2024-12-23 18:22:23 -0500

    fixed resource

--- client.ts
+++ client.ts
@@ -1,5 +1,5 @@
 import { generateClient } from "aws-amplify/api";
-import { Schema } from "../amplify/data/resource";
+import { Schema } from "./amplify/data/resource";
 import { createAIHooks } from "@aws-amplify/ui-react-ai";
 
 export const client = generateClient<Schema>({ authMode: "userPool" });
